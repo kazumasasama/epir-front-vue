de4a94fb3723afe40af9edc60c1cf627fd2cbdb0
Kaz <takaik@yahoo.com>
2022-06-04 09:45:01 -0400

    translate to Japanese / LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,26 +1,22 @@
 <template>
   <div class="container">
     <div>
-      <h4>Login</h4>
+      <h4>ログイン</h4>
     </div>
     <div class="row">
       <div class="col-sm-12 login-form-container">
         <div class="login-form-container">
           <form v-on:submit.prevent="login()">
-            <small>Email:</small>
+            <small>メールアドレス</small>
             <input type="text" v-model="user.email" class="form-control">
-            <small>Password:</small>
+            <small>パスワード</small>
             <input type="password" v-model="user.password" class="form-control">
             <div class="btn-container">
-              <button type="button" class="btn btn-secondary" @click="toHome()">Back to Home</button>
-              <button type="submit" class="btn btn-primary">Login</button>
+              <button type="button" class="btn btn-secondary" @click="toHome()">ホームに戻る</button>
+              <button type="submit" class="btn btn-primary">ログイン</button>
             </div>
           </form>
         </div>
-      </div>
-      <div class="col-sm-12 login-hint">
-        <p class="login-hint-item"><small>Email: test@test.com</small></p>
-        <p class="login-hint-item"><small>Password: password</small></p>
       </div>
     </div>
   </div>
@@ -72,12 +68,5 @@ import axios from 'axios'
   }
   .btn-container {
     margin-top: 20px;
-  }
-  .login-hint {
-    margin-top: 20px;
-    text-align: center;
-  }
-  .login-hint-item {
-    margin-bottom: 8px;
   }
 </style>
